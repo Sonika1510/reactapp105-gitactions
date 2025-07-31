f1c9d63d451b7961e1c66ecbdfadfbc60c74b4fd
Sonika <pentisonika@gmail.com>
2025-07-31 12:35:59 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S105</h1>
       <p>CI/CD and Cloud Devops Course</p>
       <p>This react app is being deployed using github actions </p>
+      <h1>done with git pages and git actions</h1>
     </div>
   );
 }
