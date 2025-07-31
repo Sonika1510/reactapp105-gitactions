fff74c787d6249708597f7317e8a31c3425443d0
Sonika <pentisonika@gmail.com>
2025-07-31 11:52:57 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - S105</h1>
       <p>CI/CD and Cloud Devops Course</p>
-      <p>This is simple homepage deployed on Github Pages using Vite</p>
+      <p>This react app is being deployed using github actions </p>
     </div>
   );
 }
-
 export default App;
